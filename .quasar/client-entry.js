59019039895efe8a94e00759b66c692d81739756
kristiansen616 <52787766+kristiansen616@users.noreply.github.com>
2020-09-14 14:15:31 -0300

    teste8

--- .quasar/client-entry.js
+++ .quasar/client-entry.js
@@ -42,18 +42,12 @@ import qboot_Booti18n from 'boot/i18n'
 
 
 
-Vue.config.devtools = true
-Vue.config.productionTip = false
-
-
-
-console.info('[Quasar] Running SPA.')
 
 
 
 
 
-const publicPath = `/`
+const publicPath = ``
 
 
 async function start () {
